1b2d75e8f5f72e2a5cfdc8c180f2a26f83be4776
coco.bike <444503829@qq.com>
2020-11-19 16:14:57 +0800

    修改名称

--- src/BiKe.Poetry.EntityFrameworkCore/EntityFrameworkCore/PoetyFreeSqlDbContext.cs
+++ src/BiKe.Poetry.EntityFrameworkCore/EntityFrameworkCore/PoetyFreeSqlDbContext.cs
@@ -7,14 +7,14 @@ namespace BiKe.Poetry.EntityFrameworkCore
 {
     public static class PoetyFreeSqlDbContext
     {
-        public static IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+        public static IFreeSql FsqlDb = new FreeSql.FreeSqlBuilder()
                                    .UseConnectionString(FreeSql.DataType.PostgreSQL, ConfigurationSetting.ConnectionString)
                                    .UseAutoSyncStructure(true) //自动迁移实体的结构到数据库
                                    .Build(); //请务必定义成 Singleton 单例模式
 
         public static IFreeSql GetFreeSql()
         {
-            return fsql;
+            return FsqlDb;
         }
 
     }
