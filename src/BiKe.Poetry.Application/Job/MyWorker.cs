bba5f6af3867e6a0b3cdcaa817dc8f2edb55bee0
coco.bike <444503829@qq.com>
2021-01-19 16:07:45 +0800

    升级版本

--- src/BiKe.Poetry.Application/Job/MyWorker.cs
+++ src/BiKe.Poetry.Application/Job/MyWorker.cs
@@ -7,40 +7,40 @@ using Volo.Abp.Threading;
 
 namespace BiKe.Poetry.Job
 {
-    public class MyWorker : AsyncPeriodicBackgroundWorkerBase
-    {
-        public MyWorker(
-            AbpTimer timer,
-            IServiceScopeFactory serviceScopeFactory
-        ) : base(
-            timer,
-            serviceScopeFactory)
-        {
-            Timer.Period = 1000; //10 minutes
-        }
+    //public class MyWorker : AsyncPeriodicBackgroundWorkerBase
+    //{
+    //    public MyWorker(
+    //        AbpTimer timer,
+    //        IServiceScopeFactory serviceScopeFactory
+    //    ) : base(
+    //        timer,
+    //        serviceScopeFactory)
+    //    {
+    //        Timer.Period = 1000; //10 minutes
+    //    }
 
 
-        protected override async Task DoWorkAsync(
-        PeriodicBackgroundWorkerContext workerContext)
-        {
-            Logger.LogInformation("Starting: Setting status of inactive users...");
+    //    protected override async Task DoWorkAsync(
+    //    PeriodicBackgroundWorkerContext workerContext)
+    //    {
+    //        Logger.LogInformation("Starting: Setting status of inactive users...");
 
-            //Resolve dependencie
-            var authorService = workerContext.ServiceProvider.GetRequiredService<IAuthorAppService>();
-            authorService.TestJob();
-            //Do the work
+    //        //Resolve dependencie
+    //        var authorService = workerContext.ServiceProvider.GetRequiredService<IAuthorAppService>();
+    //        authorService.TestJob();
+    //        //Do the work
 
-            Logger.LogInformation("Completed: Setting status of inactive users...");
-        }
+    //        Logger.LogInformation("Completed: Setting status of inactive users...");
+    //    }
 
-        //public override Task StartAsync(CancellationToken cancellationToken = default)
-        //{
-        //    //...
-        //}
+    //    //public override Task StartAsync(CancellationToken cancellationToken = default)
+    //    //{
+    //    //    //...
+    //    //}
 
-        //public override Task StopAsync(CancellationToken cancellationToken = default)
-        //{
-        //    //...
-        //}
-    }
+    //    //public override Task StopAsync(CancellationToken cancellationToken = default)
+    //    //{
+    //    //    //...
+    //    //}
+    //}
 }
